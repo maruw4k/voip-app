4234dacc89f6cd0f5fc22022bcbec9036a7a2da4
Marek Krzeszowiec <maruw4k@gmail.com>
2018-03-22 15:32:53 +0100

    Dodanie panelu rozmowy i okienka statusu

--- angular/app/components/dashboard/dashboard.component.js
+++ angular/app/components/dashboard/dashboard.component.js
@@ -3,14 +3,21 @@ class DashboardController {
     'ngInject'
 
 
+    var _this = this;
+    this.info = {
+      status: 'Łączenie',
+      textBtn: 'Zarejestruj'
+    }
+
+
     $scope.onClick = function () {
       alert('Dupa')
     }
 
 
     var ua = new SIP.UA({
-      uri: 'sip:testuser@192.168.43.21',
-      wsServers: ['ws://192.168.43.21:80/mfstwebsock'],
+      uri: 'sip:testuser@192.168.0.17',
+      wsServers: ['ws://192.168.0.17:80/mfstwebsock'],
       authorizationUser: 'testuser',
       password: '7770751389206',
       traceSip: true,
@@ -28,16 +35,46 @@ class DashboardController {
 
   ua.on('connected', function () {
       console.log('Connected');
-
+      _this.info.status = 'Połączony';
   });
 
   ua.on('registered', function () {
-    var subscription = ua.subscribe('sip:1111@192.168.43.21', 'presence');
-
-
+    var subscription = ua.subscribe('sip:1111@192.168.0.17', 'presence');
       console.log('registered');
+      _this.info.status = 'Zarejestrowany';
 
   });
+
+
+  $scope.registerSIP = function () {
+    console.log('METODAAAAAAAAAAAAAA');
+    if (!ua) return;
+
+    if (ua.isRegistered()) {
+        _this.info.textBtn = 'Zarejestruj';
+        ua.unregister();
+        _this.info.status = 'Wylogowano';
+    } else {
+        _this.info.textBtn = 'Wyloguj';
+        ua.register();
+        _this.info.status = 'Zarejestrowano';
+    }
+}
+
+//Dzwonienie
+$scope.call = function () {
+    $scope.status_info = 'Dzwonienie';
+    simple.call('marek@wwsi.onsip.com');
+    // simple.call('recipientUri.value');
+}
+
+
+//Zakończenie połączenia
+// endButton.addEventListener("click", function (e) {
+//     $scope.status_info = 'Zakończono';
+//     simple.hangup();
+// }, false);
+
 
   }
 }
